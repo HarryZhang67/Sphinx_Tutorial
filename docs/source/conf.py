d740a61cfb6c9d896eeeda5e30cebc9394b678f6
wzhan564 <wzhan564@biodatsci.uwo.ca>
2024-05-17 14:32:35 -0400

    Initial commit of Sphinx project

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+import pathlib
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Lumache'
+copyright = '2024, Graziella'
+author = 'Graziella'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+# If you want to avoid converting .rst to .txt
+html_copy_source = True  # Ensure this is True to copy the source files for viewing
+html_sourcelink_suffix = ''  # Set to empty to keep .rst extensions
